c5ef3555a8479586106b502f7363599e93573e75
Tieu Ha Anh Khoi _ K14 FUG CT <khoithace140252@fpt.edu.vn>
2021-03-07 17:18:37 +0700

    API005 [KhoiTHA]

--- api/server/routes.py
+++ api/server/routes.py
@@ -8,11 +8,28 @@ import jwt
 # from server.until import *
 from datetime import date, datetime, timedelta
 from server.msg import error, success
+from functools import wraps
 
-@app.route("/ping", methods=["GET"])
-def ping():
-    return "ping"
-    
+# check token
+def check_for_token(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        token = request.headers['Authentication']
+        if not token:
+            Logout()
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            Logout()
+        return func(*args, **kwargs)
+    return wrapped
+
+#Đăng xuất
+@app.route("/logout", methods=["GET"])
+def Logout():
+    session['logged_in'] = False
+    return "logout"
+
 #api001
 @app.route("/detect", methods=["POST"])
 def detect():
@@ -105,21 +122,74 @@ def login():
         username = request.values["username"]
         password = hashlib.md5(request.values["password"].encode()).hexdigest()
         records = Admins.query.filter_by(username=username, password=password).first()
-        if records:
-            token = jwt.encode({
-                'user': username,
-                'exp': datetime.now() + timedelta(seconds=1440)
-            }, app.config['SECRET_KEY'])
-            session['logged_in'] = True
-    #Step 2 : Xử lý kết quả xác thực
     except:
         return jsonify(
             messages=error["handleFailure"],
             success=False
         ), status.HTTP_400_BAD_REQUEST
-    
+    #Step 2 : Xử lý kết quả xác thực
+    if records:
+        token = jwt.encode({
+            'user': username,
+            'exp': datetime.now() + timedelta(seconds=1440)
+        }, app.config['SECRET_KEY'])
+        session['logged_in'] = True
+    else:
+        return jsonify(
+            messages=error["AuthenticationFailure"],
+            success=False
+        ), status.HTTP_400_BAD_REQUEST
     #Step 3 : Kiểm tra thông tin đăng nhập
     return jsonify(
             token=token.decode('utf-8'),
+            success=True
+        ), status.HTTP_200_OK
+#api004
+@app.route("/insertBenh", methods=["POST"])
+#Step 1 : Check user token
+@check_for_token
+def insertBenh():
+    #Step 2 : Tạo bệnh mới
+    try:
+        insertBenh = Benh(Ten_B=request.values["Ten_B"],ThongTin_B=request.values["ThongTin_B"],CachChuaTri=request.values["CachChuaTri"],GhiChu=request.values["GhiChu"],Created=datetime.now(),Updated=datetime.now(),Created_function_id="api004",Updated_function_id="api004",Revision=0,TrangThai=True)
+        db.session.add(insertBenh)
+        db.session.commit()
+    except:
+        return jsonify(
+            messages=error["HandleFailure"],
+            success=False
+        ), status.HTTP_400_BAD_REQUEST
+    #Step 3 : Trả về kết quả xử lý
+    return jsonify(
+            Records=1,
+            success=True
+        ), status.HTTP_200_OK
+#api005
+@app.route("/updateBenh", methods=["PATCH"])
+#Step 1 : Check user token
+@check_for_token
+def updateBenh():
+    #Step 2 : Tạo bệnh mới
+    try:
+        Id_B = request.values["Id_B"]
+        benh = Benh.query.filter_by(Id_B=Id_B).first()
+        benh.Ten_B=request.values["Ten_B"]
+        benh.ThongTin_B=request.values["ThongTin_B"]
+        benh.CachChuaTri=request.values["CachChuaTri"]
+        benh.GhiChu=request.values["GhiChu"]
+        benh.Updated=datetime.now()
+        benh.Updated_function_id="api005"
+        benh.Revision+=1
+        benh.TrangThai=True
+        benh.Id_B=Id_B
+        db.session.commit()
+    except:
+        return jsonify(
+            messages=error["handleFailure"],
+            success=False
+        ), status.HTTP_400_BAD_REQUEST
+    #Step 3 : Trả về kết quả xử lý
+    return jsonify(
+            Records=1,
             success=True
         ), status.HTTP_200_OK
