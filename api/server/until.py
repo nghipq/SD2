1e434f158c50cdbfa939d39bd22bd94bc2a3e3d3
nghipq <47938866+nghipq@users.noreply.github.com>
2021-11-28 21:47:37 +0700

    Update until.py

--- api/server/until.py
+++ api/server/until.py
@@ -12,8 +12,8 @@ from datetime import date, datetime, timedelta
 from server import app, db
 from server.models import Admins, adminSchema, adminsSchema, Model, modelSchema, modelsSchema, Benh, benhSchema, benhsSchema, ModelBenh, modelSchema, modelsSchema, ModelBenh, modelBenhSchema, modelBenhsSchema, NhanDien, nhanDienSchema, nhanDiensSchema
 
-vectors_file = [("training_set_dom_den.csv", 0), ("training_set_dom_trang.csv", 1), ("training_set_den_mang.csv", 0), 
-("training_set_hoai_tu_co.csv", 1), ("training_set_hoai_tu_gan.csv", 0), ("training_set_tom_bt.csv", 2)]
+vectors_file = [("training_set_dom_den.csv", 2), ("training_set_dom_trang.csv", 2), ("training_set_den_mang.csv", 2), 
+("training_set_hoai_tu_co.csv", 2), ("training_set_hoai_tu_gan.csv", 2), ("training_set_tom_bt.csv", 2)]
 kmeans = []
 
 for File, clr in vectors_file:
@@ -26,30 +26,25 @@ def detect_tom_desease(img, Ten_M):
     surf = cv2.xfeatures2d.SURF_create(5000)
 
     with open(f"./server/models/{Ten_M}.pickle", "rb") as file:
+      try:
         clf = pickle.load(file)
 
-        image = cv2.imread(os.path.join("./server/img", img))
+        image = cv2.imread(img)
         image = cv2.resize(image, (256, 128))
 
         _, des = surf.detectAndCompute(image,None)
 
         ans = []
-
-        for i in range(len(kmeans)):
-            rs = kmeans[i][0].predict(des)
-            data = []
-            for j in range(len(des)):
-                if rs[j] == kmeans[i][1]:
-                    data.append(des[j])
-            if len(data) > 0:
-                pre = clf.predict(data)
-                dect = int(np.argmax(np.bincount(pre.astype(int))))
-                if(dect == i):
-                    ans.append(dect)
-
-        print(ans)
-        if len(ans) > 0:
-            return ans[0]
+        rs = kmeans[1][0].predict(des)
+        data = []
+        for j in range(len(des)):
+            if rs[j] == kmeans[1][1]:
+                data.append(des[j])
+        if len(data) > 0:
+            pre = clf.predict(data)
+            dect = int(np.argmax(np.bincount(pre.astype(int))))
+        return dect
+      except:
         return -1
 
 #Get query from DB
